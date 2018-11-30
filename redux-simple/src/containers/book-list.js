f1ef8c08ced661904a78fd05f0342ce507cd074b
truongbt <bui.tuan.truong@framgia.com>
2018-11-30 14:13:00 +0700

    finish action reducers

--- redux-simple/src/containers/book-list.js
+++ redux-simple/src/containers/book-list.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import {selectBook} from '../action/index';
-import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       )
     });
   }
@@ -35,7 +40,8 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({selectBook: selectBook}, dispatch);
 }
 
-
+// Promote BookList from a component to a container - it needs to know about this new dispatch method, SelectBook
+// Make it available as a prop
 export default connect(
   mapStateToProps,
   mapDispatchToProps
