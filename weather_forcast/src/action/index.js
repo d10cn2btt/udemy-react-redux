2bdf210d409e5737e4de247a20a5d891f28da24b
truongbt <bui.tuan.truong@framgia.com>
2018-12-03 17:09:42 +0700

    add middleware, call ajax via axios

---
+++ weather_forcast/src/action/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY_WEATHER = '45a89bd210a3a9d46aea7273b3645e90';
+const URL_WEATHER = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY_WEATHER}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const request = axios.get(`${URL_WEATHER}&q=${city},vn`);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  }
+}
---
+++ weather_forcast/src/containers/search_bar.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+import {fetchWeather} from '../action';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
+  onInputChange(event, extraParam) {
+    console.log(extraParam);
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+  }
+
+  render() {
+    return (
+      <form onSubmit={() => this.onFormSubmit(event)} className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          value={this.state.term}
+          onChange={() => this.onInputChange(event, 'ExtraParam')}
+        />
+        <span className="input-group-btn">
+          <button className="btn btn-primary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SearchBar);
