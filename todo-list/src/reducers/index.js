6e89f3b5c871e3f9cb39aa6a8144efcb8855e3fc
d10cn2btt <truong123>
2019-02-12 00:20:47 +0700

    separate reducers

--- todo-list/src/reducers/index.js
+++ todo-list/src/reducers/index.js
@@ -1,69 +1,9 @@
 import {combineReducers} from 'redux';
 
-// import todoApp from './todo_list';
-//
-// export default combineReducers({
-//   todoApp,
-// })
+import todoApp from './todo_list';
+import filterApp from './todo_filter';
 
-import {
-  ADD_TODO,
-  TOGGLE_TODO,
-  SET_VISIBILITY_FILTER,
-  VisibilityFilters
-} from '../actions/actionTypes'
-
-const {SHOW_ALL} = VisibilityFilters
-
-function visibilityFilter(state = SHOW_ALL, action) {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter
-    default:
-      return state
-  }
-}
-
-function todos(state = [], action) {
-  console.log(action.type);
-
-  switch (action.type) {
-    // case SET_VISIBILITY_FILTER:
-    //   console.log('set visibility');
-    //   return Object.assign({}, state, {
-    //     visibilityFilter: action.filter
-    //   });
-
-    case TOGGLE_TODO:
-      console.log('toggle');
-      return state.map((todo, index) => {
-        if (index === action.index) {
-          return Object.assign({}, todo, {
-            completed: !todo.completed
-          })
-        }
-        return todo
-      })
-
-    case ADD_TODO:
-      console.log('ccc');
-      return [
-        ...state,
-        {
-          text: action.text,
-          completed: false
-        }
-      ]
-
-    default:
-      console.log('default');
-      return state
-  }
-}
-
-const todoApp = combineReducers({
-  // visibilityFilter,
-  todos
+export default combineReducers({
+  todoApp,
+  filterApp
 })
-
-export default todoApp
--- todo-list/src/reducers/todo_list.js
+++ todo-list/src/reducers/todo_list.js
@@ -1,18 +1,6 @@
-import {commbineReducers} from 'redux';
+import {combineReducers} from 'redux';
 
-import {
-  VisibilityFilters,
-  SET_VISIBILITY_FILTER,
-  ADD_TODO,
-  TOGGLE_TODO
-} from '../actions/actionTypes';
-
-const {SHOW_ALL} = VisibilityFilters;
-
-const initialState = {
-  visibilityFilter: SHOW_ALL,
-  todos: [],
-};
+import {ADD_TODO, TOGGLE_TODO} from '../actions/actionTypes';
 
 function todos(state = [], action) {
   switch (action.type) {
@@ -25,6 +13,7 @@ function todos(state = [], action) {
         }
       ];
 
+    // never assign to anything inside the state unless you clone it first.
     case TOGGLE_TODO:
       return state.map((todo, index) => {
         if (index === action.index) {
@@ -35,13 +24,6 @@ function todos(state = [], action) {
 
         return todo;
       });
-  }
-}
-
-function visibilifyFilter(state = SHOW_ALL, action) {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter;
 
     default:
       return state;
@@ -52,36 +34,22 @@ function visibilifyFilter(state = SHOW_ALL, action) {
  * Redux will call our reducer with an undefined state for the first time. This is our chance to return the initial state of our app
  * The reducer is a pure function that takes the previous state and an action, and returns the next state.
  * (previousState, action) => newState
+ *
+ * Each of these reducers is managing its own part of the global state.
+ * The state parameter is different for every reducer, and corresponds to the part of the state it manages.
  */
-export default function todoApp(state = initialState, action) {
-  // Each of these reducers is managing its own part of the global state.
-  // he state parameter is different for every reducer, and corresponds to the part of the state it manages.
-  // return {
-  //   visibilifyFilter: visibilifyFilter(state.visibilityFilter, action),
-  //   stores: stores(state.stores, action)
-  // };
 
-  switch (action.type) {
-    // We don't mutate the `state`
-    case SET_VISIBILITY_FILTER:
-      return Object.assign({}, state, {
-        visibilityFilter: action.filter
-      });
+const todoApp = combineReducers({
+  // Move filter to file todo_filter.js
+  // visibilityFilter,
+  todos
+});
 
-    case ADD_TODO:
-      console.log('vcxxcv');
-      return Object.assign({}, state, {
-        todos: todos(state.todos, action)
-      });
+export default todoApp;
 
-    // never assign to anything inside the state unless you clone it first.
-    case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: todos(state.todos, action)
-      });
-    // We return the previous `state` in the default case. It's important to return the previous state for any unknown action.
-    default:
-      console.log('default');
-      return state;
-  }
-}
+// export default function todoApp(state = initialState, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   };
+// }
