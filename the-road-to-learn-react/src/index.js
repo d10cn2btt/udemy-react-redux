8be4b1dc231cfe3def13efbf34b2ac6766c569ad
truongbt <bui.tuan.truong@framgia.com>
2019-03-25 15:04:13 +0700

    chapter 1: declare list

--- the-road-to-learn-react/src/index.js
+++ the-road-to-learn-react/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+// https://github.com/the-road-to-learn-react/the-road-to-learn-react/blob/master/manuscript/chapter1.md#hot-module-replacement
+if (module.hot) {
+  module.hot.accept()
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
